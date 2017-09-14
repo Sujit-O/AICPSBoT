14a4c418bac9a59d60b952eb86666f2b11403f9a
Sujit-O <sujitchhetri@gmail.com>
2017-09-14 14:29:36 -0700

    Update WangoBot.py

--- WangoBot.py
+++ WangoBot.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
+ 
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
