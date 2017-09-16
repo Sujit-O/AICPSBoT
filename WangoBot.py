53c08544e24aa4583c3d466fae782b4205d74cdf
Sujit-O <sujitchhetri@gmail.com>
2017-09-15 18:31:53 -0700

    Update WangoBot.py

--- WangoBot.py
+++ WangoBot.py
@@ -93,13 +93,13 @@ def echo(bot, update):
         yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
         result = urllib.request.urlopen(yql_url).read()
         data = json.loads(result)
-        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results'])
+        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results']['channel']['location'])
         
         yql_query = "select item.condition from weather.forecast where woeid=2427665"
         yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
         result = urllib.request.urlopen(yql_url).read()
         data = json.loads(result)
-        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results'])
+        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results']['channel']['item']['condition'])
         return
      
     if  'weather' in text and 'new jersey' in text:
@@ -108,13 +108,13 @@ def echo(bot, update):
         yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
         result = urllib.request.urlopen(yql_url).read()
         data = json.loads(result)
-        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results'])
+        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results']['channel']['location'])
         
         yql_query = "select item.condition from weather.forecast where woeid=2347589"
         yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
         result = urllib.request.urlopen(yql_url).read()
         data = json.loads(result)
-        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results'])   
+        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results']['channel']['item']['condition'])   
         
         return  
      
@@ -124,13 +124,13 @@ def echo(bot, update):
         yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
         result = urllib.request.urlopen(yql_url).read()
         data = json.loads(result)
-        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results'])
+        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results']['channel']['location'])
         
         yql_query = "select item.condition from weather.forecast where woeid=2427665"
         yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
         result = urllib.request.urlopen(yql_url).read()
         data = json.loads(result)
-        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results'])
+        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results']['channel']['item']['condition'])
         return
       
     if  'have' in text and 'weekend' in text:
