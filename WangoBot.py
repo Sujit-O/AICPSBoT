1e56444bf84467df55b495c4b3cabbfb0fb9176b
Sujit-O <sujitchhetri@gmail.com>
2017-09-15 18:40:55 -0700

    Update WangoBot.py

--- WangoBot.py
+++ WangoBot.py
@@ -104,11 +104,15 @@ def echo(bot, update):
       
     if  'jiang' in text and 'sick' in text:
         baseurl = "https://query.yahooapis.com/v1/public/yql?"
-       yql_query = "select item.condition from weather.forecast where woeid=2427665"
+        yql_query = "select item.condition from weather.forecast where woeid=2427665"
         yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
         result = urllib.request.urlopen(yql_url).read()
         data = json.loads(result)
-        bot.send_message(chat_id=update.message.chat_id, text= 'Well temp in new jersey is '+ data['query']['results']['channel']['item']['condition']['temp']+'F and weather is '+data['query']['results']['channel']['item']['condition']['text']+'. He might be having cold!'   )
+        bot.send_message(chat_id=update.message.chat_id, text= 'Well temp in new jersey is ')
+        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results']['channel']['item']['condition']['temp'])
+        bot.send_message(chat_id=update.message.chat_id, text= 'F and weather is ')
+        bot.send_message(chat_id=update.message.chat_id, text= data['query']['results']['channel']['item']['condition']['text'])
+        bot.send_message(chat_id=update.message.chat_id, text= '. He might be having cold!')
         return  
      
     if  'weather' in text and 'new jersey' in text:
